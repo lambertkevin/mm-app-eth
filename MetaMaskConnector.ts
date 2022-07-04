1349ecee9a20e919d8b185d3a924e24a692c48c9
lambertkevin <kevin.lambert@ledger.fr>
2022-07-04 16:52:57 +0200

    Chage client path resolution

--- MetaMaskConnector.ts
+++ MetaMaskConnector.ts
@@ -22,7 +22,10 @@ export class MetaMaskConnector {
 
   async start(): Promise<void> {
     this._app = express();
-    this._app.use(express.static(path.resolve(__dirname, 'client')));
+    console.log(path.join(__dirname, 'client'));
+    console.log(path.join(path.resolve('.'), 'client'));
+    console.log(path.resolve('./client'));
+    this._app.use(express.static(path.join(__dirname, 'client')));
     this._wss = await this._runServer();
     await this._initialize();
   }
